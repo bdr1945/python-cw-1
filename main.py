095036587a47c0b9ef2910ec070f2b0eef6a23fb
bdr1945 <142620983+bdr1945@users.noreply.github.com>
2023-08-24 15:11:57 +0300

    dd

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ width = 3
 area = length*width
 print( f"the result is {area}")
 # BONUS: Print the area in the following syntax: "If the length of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
-print("if the length of a rectangle is 5 and the width is 3 then the area is 15")
+print(F"if the length of a rectangle is {length} and the width is {width} then the area is {area}")
